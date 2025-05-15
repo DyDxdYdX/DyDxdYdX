fbd9d8f3f460514262935532f34afae533e99522
Dexter Dykes <dexterdykestimothy@gmail.com>
2025-05-15 21:47:52 +0800

    Configure base path and asset prefix for deployment; update build script to include export.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const repo = 'DyDxdYdX';
 const nextConfig = {
   output: 'export',
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
   images: {
     unoptimized: true,
   }
